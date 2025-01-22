b7b9ac4a5eca89bb3a240124e75709072be7617c
DeMarDerozan11 <henryba64@gmail.com>
2025-01-22 21:49:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-echo "Le site est en ligne et fonctionne correctement !";
+<?php
+require_once '/Users/irvinysl/Documents/site/conf.php';
+
+try {
+    $stmt = $pdo->query("SELECT 1");
+    echo "Connexion à la base de données réussie !";
+} catch (PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
+}
+
+?>
+
 ?>
