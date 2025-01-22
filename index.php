2d1531ba2a8785d1019167d9bf3a8af75c783de6
DeMarDerozan11 <henryba64@gmail.com>
2025-01-22 22:38:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,19 +2,15 @@
 // Inclure conf.php pour toutes les pages
 require_once '/Users/irvinysl/site/conf.php';
 
-// Récupérer le chemin de la page demandée
+// Récupérer le fichier demandé
 $page = isset($_GET['page']) ? $_GET['page'] : 'APP.php';
 
-// Sécuriser le chemin pour éviter l'accès à des fichiers non désirés
-$page = basename($page); // Extrait uniquement le nom du fichier
-
-// Vérifie si le fichier existe avant de l'inclure
+// Vérifie si le fichier demandé existe
 if (file_exists($page)) {
     require_once $page;
 } else {
     http_response_code(404);
     echo "Page non trouvée.";
 }
-
 
 ?>
