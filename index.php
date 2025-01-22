ddce159b98ae81527a03beef960de69902d40634
DeMarDerozan11 <henryba64@gmail.com>
2025-01-22 22:00:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php
-<?php
-require_once '/Users/irvinysl/Dowloads/conf.php';
-
-try {
-    $stmt = $pdo->query("SELECT 1");
-    echo "Connexion à la base de données réussie !";
-} catch (PDOException $e) {
-    echo "Erreur de connexion : " . $e->getMessage();
-}
-
-?>
+header('Location: APP.php');
+exit;
 
 ?>
