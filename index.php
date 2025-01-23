8ddb59e21afa6ea96baf49317f19de9f6e1b81dd
DeMarDerozan11 <henryba64@gmail.com>
2025-01-23 02:26:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-// Redirection vers APP.php
-header('Location: APP.php');
-exit;
+// Vérifie si le fichier APP.php existe
+if (file_exists('APP.php')) {
+    // Redirection vers APP.php
+    header('Location: APP.php');
+    exit;
+} else {
+    // Affiche un message d'erreur si APP.php est introuvable
+    echo "Erreur : Le fichier APP.php est introuvable.";
+    exit;
+}
 ?>
