1735fd6ec4fd7c72d4e985b06153752944e3e636
DeMarDerozan11 <henryba64@gmail.com>
2025-01-23 03:08:07 +0100

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,6 +1,6 @@
 <?php
-$host = getenv("herogu.garageisep.com");
-$user = getenv("23eBbNoI6B_venus");
-$pass = getenv("gv6TuEbPKUDFsEjY");
-$db = getenv("no1hbDJ3Pm_venus");
+$host = getenv('MYSQL_HOST');         // Récupère la valeur de MYSQL_HOST
+$user = getenv('MYSQL_USER');         // Récupère la valeur de MYSQL_USER
+$pass = getenv('MYSQL_PASSWORD');     // Récupère la valeur de MYSQL_PASSWORD
+$db = getenv('MYSQL_DATABASE');       // Récupère la valeur de MYSQL_DATABASE
 ?>
