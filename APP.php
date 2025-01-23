93738fab1a385b33978f3d0057fa079716f953f9
DeMarDerozan11 <henryba64@gmail.com>
2025-01-23 03:47:04 +0100

    Update APP.php

--- APP.php
+++ APP.php
@@ -30,7 +30,7 @@ try {
     <link rel="stylesheet" href="APP.css">
 </head>
 <body>
-    <?php include('Barre de recherche.php') ?>
+    <?php include('barre de recherche.php') ?>
 
 
     <div class="content-box">
