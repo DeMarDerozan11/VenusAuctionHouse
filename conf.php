5f29e49f92f86c14a0cb5a3d31d6ed3dabc4f6b0
DeMarDerozan11 <henryba64@gmail.com>
2025-01-23 02:35:36 +0100

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,5 +1,5 @@
 <?php
-$host = getenv("127.0.0.1");
+$host = getenv("herogu.garageisep.com");
 $user = getenv("23eBbNoI6B_venus");
 $pass = getenv("gv6TuEbPKUDFsEjY");
 $db = getenv("no1hbDJ3Pm_venus");
